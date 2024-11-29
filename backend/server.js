4356063467334f93d814bafa7012f13c226497dc
Sourasish01 <bhotto2023@gmail.com>
2024-11-29 21:51:54 +0530

    Build Issue Fixed 1

--- backend/server.js
+++ backend/server.js
@@ -231,10 +231,10 @@ app.post("/api/messages/send/:id", protectRoute, async (req, res) => { // to sen
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
